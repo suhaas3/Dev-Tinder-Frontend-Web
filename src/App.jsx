6b29301f5bbbfc4086fa06f52247af343536a411
suhaas3 <suhaas3w@gmail.com>
2025-09-20 19:30:33 +0530

    basic router in dev tinder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './style.css';
 import './index.css'
 import Navbar from './Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Profile from './Profile';
+import Body from './Body';
 
 
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body/>}>
+          <Route path="/login" element={<Login/>} />
+          <Route path='/profile' element={<Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <>
       <Navbar/>
+      <Outlet/>
     </>
   )
 }
