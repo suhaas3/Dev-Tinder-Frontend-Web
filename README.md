893c2e4775d986bf4d72fd1c13be5de1f2421f64
suhaas3 <suhaas3w@gmail.com>
2025-09-24 07:32:54 +0530

    feed store is created in redux toolkit

--- README.md
+++ README.md
@@ -22,7 +22,9 @@
 - Refactor our code to add constants file + create a components folder
 - You should not be access other routes without login
 - If token is not present, redirect user to login page
-
+- Logout feature
+- Get the feed and add the feed in the store
+- build the user card on feed
 
 
 
--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react';
+import {BASE_URL} from "../uitils/constants";
+import { useDispatch, useSelector } from 'react-redux';
+import { addFeed } from '../uitils/feedSlice';
+
 
 const Feed = () => {
+
+  const feed = useSelector(store => store.feed);
+
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials:true
+      });
+      dispatch(addFeed(res?.data.data));
+    }
+    catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getFeed();
+  }, [])
+
   return (
     <>
-      <h1>Feed</h1>
+
+    <h1>Feed feed</h1>
+
     </>
   )
 }
