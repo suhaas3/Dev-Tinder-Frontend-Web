aac175b3c4a14fbad7bff926d565be6d31ae798c
suhaas3 <suhaas3w@gmail.com>
2025-10-04 08:38:40 +0530

    remove default emailId and password

--- src/Components/Connections.jsx
+++ src/Components/Connections.jsx
@@ -27,7 +27,7 @@ const Connections = () => {
 
   if (!connections) return;
 
-  if (connections.length === 0) return <h1>No connections Found</h1>
+  if (connections.length === 0) return <h1 className='text-center my-10'>No connections Found</h1>
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,8 +6,8 @@ import { BASE_URL } from '../uitils/constants';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("saisuhaas@gmail.com");
-  const [password, setPassword] = useState("Saisuhaas@123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [isOpenLogin, setIsOpenLogin] = useState(false);
